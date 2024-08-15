b0db788c6327a2ec7184eb80ca59f90f9ea6975b
David Watson <davidpeterwatson@gmail.com>
2024-08-15 18:21:37 +1200

    more calcs

--- aerodynamic_calculations/aerodynamic_calculations.py
+++ aerodynamic_calculations/aerodynamic_calculations.py
@@ -1,8 +1,32 @@
+import math
+
+
 def calculate_aspect_ratio(wingspan, area):
     return wingspan * wingspan / area
+
 
 def calculate_taper_ratio(root_chord, tip_chord):
     return tip_chord / root_chord
 
+
 def calculate_ideal_taper_ratio():
     return 0.4
+
+
+def calculate_tip_chord(root_chord, taper_ratio):
+    return root_chord * taper_ratio
+
+
+# https://www.airfieldmodels.com/information_source/math_and_science_of_model_aircraft/formulas/mean_aerodynamic_chord.htm
+def calculate_mean_aerodynamic_chord(root_chord, taper_ratio):
+    return root_chord * 2 / 3 * ((1 + taper_ratio + pow(taper_ratio, 2)) / (1 + taper_ratio))
+
+
+def calculate_mean_aerodynamic_chord_distance(wingspan, root_chord, mean_aerodynamic_chord, tip_chord):
+    return wingspan / 2 * (root_chord - mean_aerodynamic_chord) / (root_chord - tip_chord)
+
+
+def calculate_sweep(nuetral_point, mean_aerodynamic_chord_distance, mean_aerodynamic_chord):
+    mean_chord_leading_edge_point = nuetral_point - 1 / 4 * mean_aerodynamic_chord
+    return math.atan(mean_chord_leading_edge_point / mean_aerodynamic_chord_distance)
+    
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -13,37 +13,76 @@ def test_design():
             'max-altitude': 500, # meters
             'max-payload': 1, # kilograms
             'max-net-weight': 2, # kilograms
-            'root-chord': 0.4 # meters
+            'max_root-chord': 0.4, # meters
+            'max_wingspan': 1.2 # meters
         }
     }
     return design_requirements
 
 def test_aspect_ratio_calculation():
+    # given
     wingspan = 10
     chord = 2
     area = chord * wingspan
+    # when
     aspect_ratio = aerodynamic_calculations.calculate_aspect_ratio(wingspan, area)
+    # then
     assert aspect_ratio == 5
 
 
 def test_taper_ratio_calculation():
+    # given
     root_chord = 2
     tip_chord = 1
+    # when
     taper_ratio = aerodynamic_calculations.calculate_taper_ratio(root_chord, tip_chord)
+    # then
     assert taper_ratio == 0.5
 
 
 def test_ideal_taper_ratio_calculation():
+    # when
     taper_ratio = aerodynamic_calculations.calculate_ideal_taper_ratio()
+    # then
     assert taper_ratio == 0.4
 
-# def test_tip_chord_length_calculation():
-#     root_chord_length = 400
-#     tip_chord_length = aerodynamic_calculations.calculate_tip_chord_length(root_chord_length)
-#     assert tip_chord_length = 
+
+def test_tip_chord_calculation():
+    # given
+    root_chord = 400
+    taper_ratio = 0.4
+    # when
+    tip_chord_length = aerodynamic_calculations.calculate_tip_chord(root_chord, taper_ratio)
+    # then
+    assert tip_chord_length == 160
 
 
+def test_calculate_mean_aerodynamic_chord():
+    root_chord = 110
+    tip_chord = 60
+    taper_ratio = tip_chord / root_chord
+    mean_aerodynamic_chord = aerodynamic_calculations.calculate_mean_aerodynamic_chord(root_chord, taper_ratio)
+    assert round(mean_aerodynamic_chord, 3) == 87.451
+
+
+def test_calculate_mean_aerodynamic_chord_distance():
+    wingspan = 600
+    root_chord = 110
+    mean_aerodynamic_chord = 87.451
+    tip_chord = 60
+    mean_aerodynamic_chord_distance = aerodynamic_calculations.calculate_mean_aerodynamic_chord_distance(wingspan, root_chord, mean_aerodynamic_chord, tip_chord)
+    assert round(mean_aerodynamic_chord_distance, 3) == 135.294
+
+
+def test_calculate_sweep():
+    mean_aerodynamic_chord = 87.451
+    desired_nuetral_point = 120
+    mean_aerodynamic_chord_distance = 135.294
+    sweep = aerodynamic_calculations.calculate_sweep(desired_nuetral_point, mean_aerodynamic_chord_distance, mean_aerodynamic_chord)
+    assert round(sweep, 5) == 0.62755
 
 
 # def test_aerodynamic_sweep_calculation():
 #     sweep = aerodynamic_calculations.calculate_sweep(root_chord_length, wing_span, nuetral_point)
+
+# https://dergipark.org.tr/tr/download/article-file/629766
